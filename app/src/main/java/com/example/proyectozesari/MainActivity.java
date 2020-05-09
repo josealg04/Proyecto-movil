805db86370877ac24f492f70726012f0540391d1
Jose Lopez <joseangel.lg12@gmail.com>
2020-05-09 03:43:51 -0500

    Custom navigation drawer with fragments

--- app/src/main/java/com/example/proyectozesari/MainActivity.java
+++ app/src/main/java/com/example/proyectozesari/MainActivity.java
@@ -1,10 +1,27 @@
 package com.example.proyectozesari;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +33,47 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout=findViewById(R.id.drawer);
+        toolbar=findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        toggle.getDrawerArrowDrawable().setColor(Color.parseColor("#283593"));
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView=findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id=menuItem.getItemId();
+                Fragment fragment=null;
+                switch (id){
+                    case R.id.home:
+                        fragment=new HomeFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.sitios:
+                        fragment=new SitiosFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.historias:
+                        fragment=new HistoriasFragment();
+                        loadFragment(fragment);
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame,fragment).commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        fragmentTransaction.addToBackStack(null);
     }
 }
